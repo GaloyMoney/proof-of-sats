c30cce18bb5d98baff8f1ac586b2e223a46a38bc
thevaibhavdixit <76673432+thevaibhav-dixit@users.noreply.github.com>
2022-06-02 01:34:37 +0530

    feat: add jest for testing

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  moduleFileExtensions: ["js", "json", "ts", "gql"],
+  rootDir: ".",
+  roots: ["<rootDir>/test"],
+  transform: {
+    "^.+\\.(ts)$": "ts-jest",
+  },
+  testEnvironment: "node",
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.json',
+    },
+  },
+}
