1ef0bb8eb5d64471c687018cf6a7026a28247315
thevaibhavdixit <76673432+thevaibhav-dixit@users.noreply.github.com>
2022-06-02 02:51:04 +0530

    fix: shuffle now returns a new array

--- src/utils.ts
+++ src/utils.ts
@@ -1,20 +1,26 @@
 // to generate a random integer between min (inclusive) and max (inclusive).
-export const randomInt = (min: number, max: number): number => {
+export const randomInt = (min: number = 0, max: number): number => {
+    if (min > max){
+        let temp = min
+        min = max 
+        max = temp
+    }
     return Math.floor(Math.random() * (max - min + 1) + min)
 }
 
 // function to shuffle the array
 export const shuffle = <T>(arr : T[]): T[] =>{
-    let curr = arr.length
+    const result = arr.map((element)=>element)
+    let curr = result.length
     let temp
     let randIdx
     while (curr > 0) {
         curr--
         randIdx = Math.floor(Math.random() * curr)
-        temp = arr[curr]
-        arr[curr] = arr[randIdx]
-        arr[randIdx] = temp
+        temp = result[curr]
+        result[curr] = result[randIdx]
+        result[randIdx] = temp
     }
-    return arr
+    return result
 }
 
