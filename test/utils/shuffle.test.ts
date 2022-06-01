52ad3494ced7c7d25bfb20bee0c7818b92fc87a2
thevaibhavdixit <76673432+thevaibhav-dixit@users.noreply.github.com>
2022-06-02 02:51:40 +0530

    feat: add test for utils

---
+++ test/utils/shuffle.test.ts
@@ -0,0 +1,12 @@
+import { shuffle } from "../../src/utils";
+
+describe("test shuffle", () => {
+    const arr = [12,'3123',312.32,'galoy',{accountId: "123asdfg", walletBalance: 123.12}];
+    it("should return a shuffled array", () => {
+        const result = shuffle(arr);
+        expect(result.length).toBe(arr.length);
+        expect(result).not.toEqual(arr)
+        expect(result).toEqual(expect.arrayContaining(arr))
+        expect(result[0]).not.toEqual(arr[0])
+    })
+})
