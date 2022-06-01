ba7d7061c9855812b471e2980d69bbd3af38ece8
thevaibhavdixit <76673432+thevaibhav-dixit@users.noreply.github.com>
2022-06-02 03:24:56 +0530

    test: add test for createLiabilities

---
+++ test/create-liabilities.test.ts
@@ -0,0 +1,15 @@
+import { createLiabilities } from "../src/create-liabilities";
+import { testAccounts } from "./helper";
+
+describe("test the liabilities function",()=>{
+    it("should return an array of liabilities",()=>{
+        const result = createLiabilities(testAccounts);
+        expect(result).toBeInstanceOf(Array);
+        expect(result).toHaveLength(Math.pow(2,Math.ceil(Math.log2(testAccounts.length*2))));
+    })
+    it("should return liabilities that have the same accountId's as present in the testAccounts",()=>{
+        const result = createLiabilities(testAccounts);
+        expect(testAccounts.map(({accountId})=>({accountId}))).toEqual(expect.arrayContaining(result.map(({accountId})=>({accountId}))));
+    })
+    
+})
