2358fed4bef79f7bd8d4e6d51b410b02057ef56e
thevaibhavdixit <76673432+thevaibhav-dixit@users.noreply.github.com>
2022-06-03 19:34:19 +0530

    docs: add better comments

--- src/create-tree.ts
+++ src/create-tree.ts
@@ -1,41 +1,57 @@
 import { createHash } from 'crypto'
-import { randomInt, shuffle } from './utils'
+import { randomInt, shuffle, nodeCombiner } from './utils'
 
 // TODO: Still have to implement the hashing with nonce and blockheight
 
-// generate the leaf containing hash and sum from liability.
-const getLeaf = (liability: Liability, idx: number): TreeNode => {
-  const data = `${liability.accountId}${liability.walletBalance}${idx}`
+/**
+ * create a leaf node from a liability by using sha256 algorithm.
+ * @param liability
+ * @param idx
+ * @returns {TreeNode}
+ */
+export const getLeaf = (liability: Liability, idx: number): TreeNode => {
+  const data = `${liability.accountId}${liability.balance}${idx}`
   return {
     hash: createHash('sha256').update(data).digest('hex'),
-    sum: liability.walletBalance,
+    sum: liability.balance,
   }
 }
 
-// Combine the two nodes to generate their parent node
-const nodeCombiner = (leftChild: TreeNode, rightChild: TreeNode): TreeNode => {
-  const data = `${leftChild.hash}${leftChild.sum}${rightChild.hash}${rightChild.sum}`
-  return {
-    hash: createHash('sha256').update(data).digest('hex'),
-    sum: leftChild.sum + rightChild.sum,
-  }
-}
+/**
+ * accept a list of accounts and return a shuffled and stretched list of liabilities
+ * @param accounts
+ * @returns {Liability[]}
+ */
 
-// create liabilities from accounts
-export const createLiabilities = (accounts: UserInfo[]): Liability[] => {
+export const createLiabilities = (accounts: Liability[]): Liability[] => {
   let liabilities = accounts.map(({ accountId, balance }) => ({
     accountId: accountId,
-    walletBalance: balance,
+    balance: balance,
   }))
   liabilities = shuffle(liabilities)
   const stretchedliabilities = getStretchedLiabilities(liabilities)
   return shuffle(stretchedliabilities)
 }
 
-// Create a merkle tree from the accounts
-export const createLiabilitiesTree = (accounts: UserInfo[]) => {
+/**
+ * generates a tree from the list of accounts containing
+ * the acconutId and balance
+ * @param accounts
+ * @returns {Array<Array<TreeNode>>}
+ */
+export const createLiabilitiesTree = (accounts: Liability[]) => {
   const liabilities = createLiabilities(accounts)
   const leaves = liabilities.map((liability, idx) => getLeaf(liability, idx))
+  const tree = generateTree(leaves)
+  return tree.reverse()
+}
+
+/**
+ * Generate a merkle tree from the leaves
+ * @param leaves
+ * @returns {Array<Array<TreeNode>>}
+ */
+export const generateTree = (leaves: TreeNode[]): Array<Array<TreeNode>> => {
   const tree = [leaves]
   let nodesInARow = leaves.length / 2
   let rowIndex = 0
@@ -51,10 +67,9 @@ export const createLiabilitiesTree = (accounts: UserInfo[]) => {
     tree.push(rowNodes)
     rowIndex++
   }
-  return tree.reverse()
+  return tree
 }
 
-// function to split the liabilities till they are equal to a power of 2
 const getStretchedLiabilities = (liabilities: Liability[]): Liability[] => {
   let finalLiabilities: Liability[] = liabilities
   const currSize = liabilities.length * 2
@@ -64,23 +79,20 @@ const getStretchedLiabilities = (liabilities: Liability[]): Liability[] => {
     const stretchedLiabilities: Liability[] = []
     let stretchedLiabilitiesLength = finalLiabilities.length
     finalLiabilities.forEach((liability) => {
-      if (
-        liability.walletBalance >= 0 &&
-        stretchedLiabilitiesLength < totalLeaves
-      ) {
+      if (liability.balance >= 0 && stretchedLiabilitiesLength < totalLeaves) {
         stretchedLiabilitiesLength++
         const val1: number = randomInt(
           0,
-          Math.floor(parseInt(liability.walletBalance.toString()))
+          Math.floor(parseInt(liability.balance.toString()))
         )
-        const val2: number = liability.walletBalance - val1
+        const val2: number = liability.balance - val1
         stretchedLiabilities.push({
           accountId: liability.accountId,
-          walletBalance: val1,
+          balance: val1,
         })
         stretchedLiabilities.push({
           accountId: liability.accountId,
-          walletBalance: val2,
+          balance: val2,
         })
       } else {
         stretchedLiabilities.push(liability)
