354c097d54ee756a2eb497cac968b9f1a893f446
thevaibhavdixit <76673432+thevaibhav-dixit@users.noreply.github.com>
2022-06-22 01:02:49 +0530

    test: updated tests to reflect new changes

--- test/create-proof.test.ts
+++ test/create-proof.test.ts
@@ -6,13 +6,16 @@ describe("Test the createProof function", () => {
   const tree = createLiabilitiesTree(testAccountsForTree)
   it("should return a LiabilityProof object", () => {
     const liabilityProof = createProof("01", tree)
+    if (liabilityProof instanceof Error) throw liabilityProof
     let calculatedBalance = 0
     liabilityProof.partialLiabilityProofs.forEach((proof) => {
       calculatedBalance += proof.balance
     })
     const expectedBalance = liabilityProof.totalBalance
     expect(liabilityProof).toBeInstanceOf(Object)
-    expect(liabilityProof.noncedAccountId).toBe("01" + tree.nonceMap.get("01"))
+    expect(liabilityProof.accountId).toBe("01")
+    expect(liabilityProof.partialLiabilityProofs).toBeInstanceOf(Array)
+    expect(liabilityProof.nonce).toBe(tree.nonceMap.get("01")!)
     expect(calculatedBalance).toBe(expectedBalance)
     expect(liabilityProof.partialLiabilityProofs.length).toBeGreaterThan(1)
   })
@@ -21,17 +24,17 @@ describe("Test the createProof function", () => {
     const liabilityProof = createProof("02", tree)
     const expectedBalance = 0
     let calculatedBalance = 0
+    if (liabilityProof instanceof Error) throw liabilityProof
     liabilityProof.partialLiabilityProofs.forEach((proof) => {
       calculatedBalance += proof.balance
     })
     expect(liabilityProof).toBeInstanceOf(Object)
-    expect(liabilityProof.noncedAccountId).toBe("02" + tree.nonceMap.get("02"))
+    expect(liabilityProof.nonce).toBe(tree.nonceMap.get("02"))
     expect(calculatedBalance).toBe(expectedBalance)
     expect(liabilityProof.partialLiabilityProofs.length).toBeGreaterThan(1)
   })
   it("should return an empty object for an invalid accountId", () => {
     const liabilityProof = createProof("123", tree)
-    expect(liabilityProof).toBeInstanceOf(Object)
-    expect(liabilityProof.partialLiabilityProofs.length).toBe(0)
+    expect(liabilityProof).toBeInstanceOf(Error)
   })
 })
--- test/is-liability-included-in-tree.test.ts
+++ test/is-liability-included-in-tree.test.ts
@@ -7,22 +7,25 @@ const roothash = tree.merkleTree[0][0].hash
 describe("test the function isLiabilityIncludedInTree", () => {
   it("should return true for a given liability proof", () => {
     const liabilityProof = createProof("03", tree)
+    if (liabilityProof instanceof Error) throw liabilityProof
     const verification = isLiabilityIncludedInTree(liabilityProof, roothash)
     expect(verification.isProofValid).toBe(true)
     expect(verification.provenBalance).toBe(liabilityProof.totalBalance)
   })
   it("should return false when a wrong accountId is given to generate the proof", () => {
     const liabilityProof = createProof("123", tree)
-    const verification = isLiabilityIncludedInTree(liabilityProof, roothash)
-    expect(verification.isProofValid).toBe(false)
-    expect(verification.provenBalance).toBe(0)
+    expect(liabilityProof).toBeInstanceOf(Error)
+    // We will not be able to generate any proof for an invalid accountId
+    // So user won't get any proof to validate
   })
   it("should return false when a wrong rootHash is given to generate the proof", () => {
     const liabilityProof = createProof("04", tree)
+    if (liabilityProof instanceof Error) throw liabilityProof
     expect(isLiabilityIncludedInTree(liabilityProof, "3124").isProofValid).toBe(false)
   })
   it("should return true for when accountId is 02", () => {
     const liabilityProof = createProof("02", tree)
+    if (liabilityProof instanceof Error) throw liabilityProof
     expect(isLiabilityIncludedInTree(liabilityProof, roothash).isProofValid).toBe(true)
     expect(isLiabilityIncludedInTree(liabilityProof, roothash).provenBalance).toBe(
       liabilityProof.totalBalance,
@@ -32,18 +35,21 @@ describe("test the function isLiabilityIncludedInTree", () => {
 describe("test the function isLiabilityIncludedInTree using the tree generated from testAccounts", () => {
   it("should return true for a valid accountId", () => {
     const liabilityProof = createProof("01", tree)
+    if (liabilityProof instanceof Error) throw liabilityProof
     const verification = isLiabilityIncludedInTree(liabilityProof, roothash)
     expect(verification.isProofValid).toBe(true)
     expect(verification.provenBalance).toBe(liabilityProof.totalBalance)
   })
   it("should return false for invalid accountId", () => {
     const liabilityProof = createProof("123", tree)
-    expect(isLiabilityIncludedInTree(liabilityProof, roothash).isProofValid).toBe(false)
+    expect(liabilityProof).toBeInstanceOf(Error)
+    // We will not be able to generate any proof for an invalid accountId
+    // So user won't get any proof to validate
   })
   it("should return false for invalid root hash", () => {
-    expect(isLiabilityIncludedInTree(createProof("01", tree), "123").isProofValid).toBe(
-      false,
-    )
+    const liabilityProof = createProof("01", tree)
+    if (liabilityProof instanceof Error) throw liabilityProof
+    expect(isLiabilityIncludedInTree(liabilityProof, "123").isProofValid).toBe(false)
   })
 })
 
