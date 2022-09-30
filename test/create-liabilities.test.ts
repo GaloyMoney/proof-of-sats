0a401f7f493596a586a9e5f2d6f043376b62ab18
Kartik Shah <krtk.6160@gmail.com>
2022-09-30 10:36:01 +0530

    chore: dummy commit [skip ci]

--- test/create-liabilities.test.ts
+++ test/create-liabilities.test.ts
@@ -9,7 +9,6 @@ describe("createLiabilities function", () => {
       Math.pow(2, Math.ceil(Math.log2(testAccountsForTree.length * 2))),
     )
   })
-
   it("should return liabilities that have the same accountId's as present in the testAccounts", () => {
     const result = createLiabilities(testAccountsForTree as Liability[])
     expect(testAccountsForTree.map(({ accountId }) => ({ accountId }))).toEqual(
